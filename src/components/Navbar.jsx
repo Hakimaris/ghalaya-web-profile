16ea8ea51ab8955a04108f0f81fd21cea0dc2c93
NdnHnnt <nadinehaninta@gmail.com>
2023-08-07 20:56:15 +0700

    del:eslint

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AiOutlineMenu from 'react-icons/ai'
+
+const Navbar = () => {
+  return (
+    <div>
+      <AiOutlineMenu></AiOutlineMenu>
+      <h1>Hhhh</h1>
+    </div>
+  );
+};
+
+export default Navbar;
