fd66d100028552377a11685087ae7076597d0c91
Nadine Haninta <nadinehaninta@gmail.com>
2023-09-01 09:47:48 +0700

    del:unused icons

--- src/components/LandingFooter.jsx
+++ src/components/LandingFooter.jsx
@@ -1,36 +1,35 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import {FaWhatsapp} from "react-icons/fa";
-import {FiMail} from "react-icons/fi";
-import {GoLocation} from "react-icons/go";
+import { FaWhatsapp } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+import { GoLocation } from "react-icons/go";
 
 const LandingFooter = () => {
-  const [t,i18n] = useTranslation("global")
+  const [t, i18n] = useTranslation("global");
   return (
     <footer className="bg-info text-white py-4 text-center">
       <ul className="flex justify-center items-center">
         <li className="pb-4 space-x-5">
-          <a href="#" className="font-semibold">{t("navbar.tentangKami")}</a>
-          <a href="#" className="font-semibold">{t("navbar.klien")}</a>
-          <a href="#" className="font-semibold">{t("navbar.sertifikasi")}</a>
-          <a href="#" className="font-semibold">{t("navbar.kontak")}</a>
+          <a href="#" className="font-semibold">
+            {t("navbar.tentangKami")}
+          </a>
+          <a href="#" className="font-semibold">
+            {t("navbar.klien")}
+          </a>
+          <a href="#" className="font-semibold">
+            {t("navbar.sertifikasi")}
+          </a>
+          <a href="#" className="font-semibold">
+            {t("navbar.kontak")}
+          </a>
         </li>
       </ul>
-      <ul className="flex justify-center items-center">
-        <li className="pb-4 space-x-5 px-6">
-          <a href="#" className="font-semibold"><FaWhatsapp className="-mr-1 h-8 w-8"/></a>
-          </li>
-        <li className="pb-4 space-x-5 px-6">
-          <a href="#" className="font-semibold"><FiMail className="-mr-1 h-8 w-8"/></a>
-          </li>
-        <li className="pb-4 space-x-5 px-6">
-          <a href="#" className="font-semibold"><GoLocation className="-mr-1 h-8 w-8"/></a> 
-        </li>
-      </ul>
-      <p>&copy; {new Date().getFullYear()} PT. Ghalaya Abdi Persada Line. All rights reserved.</p>
+      <p>
+        &copy; {new Date().getFullYear()} PT. Ghalaya Abdi Persada Line. All
+        rights reserved.
+      </p>
     </footer>
   );
-}
-
+};
 
 export default LandingFooter;
